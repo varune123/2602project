3b80ff7c7496fa50b00ef4bead5d063350500019
varune123 <varune23@hotmail.com>
2021-04-25 06:00:46 +0000

    signup complete, need to create a link from login to sign up

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ from flask_jwt import JWT, jwt_required, current_identity
 from sqlalchemy.exc import IntegrityError
 from datetime import timedelta 
 
-from models import db, Exercise, User #add application models
+
+from models import db, Exercise, User, SignUp, LogIn #add application models
 
 ''' Begin boilerplate code '''
 
 ''' Begin Flask Login Functions '''
-# login_manager = LoginManager()
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(user_id)
+login_manager = LoginManager()
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 ''' End Flask Login Functions '''
 
@@ -23,40 +24,70 @@ def create_app():
   app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
   app.config['SECRET_KEY'] = "MYSECRET"
-#   app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7) # uncomment if using flsk jwt
+  app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7) # uncomment if using flsk jwt
   CORS(app)
 #   login_manager.init_app(app) # uncomment if using flask login
   db.init_app(app)
   return app
 
 app = create_app()
+login_manager.init_app(app) 
 
 app.app_context().push()
 
 ''' End Boilerplate Code '''
 
 ''' Set up JWT here (if using flask JWT)'''
-# def authenticate(uname, password):
-#   pass
+def authenticate(uname, password):
+    #search for the specified user
+    user = User.query.filter_by(username=uname).first()
+    #if user is found and password matches
+    if user and user.check_password(password):
+        return user
 
-# #Payload is a dictionary which is passed to the function by Flask JWT
-# def identity(payload):
-#   pass
+#Payload is a dictionary which is passed to the function by Flask JWT
+def identity(payload):
+    return User.query.get(payload['identity'])
 
-# jwt = JWT(app, authenticate, identity)
+jwt = JWT(app, authenticate, identity)
 ''' End JWT Setup '''
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-  return render_template('app.html')
+  form = LogIn()
+  return render_template('login.html', form=form)
 
-@app.route('/login')
-def login():
-  return render_template('login.html')
+@app.route('/login', methods=['POST'])
+def loginAction():
+  form = LogIn()
+  if form.validate_on_submit(): # respond to form submission
+      data = request.form
+      user = User.query.filter_by(username = data['username']).first()
+      if user and user.check_password(data['password']): # check credentials
+        flash('Logged in successfully.') # send message to next page
+        login_user(user) # login the user
+        return redirect(url_for('home')) # redirect to main page if login successful
+  flash('Invalid credentials')
+  return redirect(url_for('index'))
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET'])
 def signup():
-    return render_template('signup.html')
+    form = SignUp() # create form object
+    return render_template('signup.html', form=form) # pass form object to template
+
+@app.route('/signup', methods=['POST'])
+def signupAction():
+    form = SignUp() # create form object
+    if form.validate_on_submit():
+        data = request.form # get data from form submission
+        newuser = User(username=data['username'], email=data['email']) # create user object
+        newuser.set_password(data['password']) # set password
+        db.session.add(newuser) # save new user
+        db.session.commit()
+        flash('Account Created!')# send message
+        return redirect(url_for('index'))# redirect to login page
+    flash('Error invalid input!')
+    return redirect(url_for('signup')) 
 
 @app.route('/exercises')
 def exercises():
--- models.py
+++ models.py
@@ -1,9 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, EqualTo, Email
+from wtforms.fields.html5 import EmailField
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
 db = SQLAlchemy()
 import datetime
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -45,3 +52,15 @@ class Exercise(db.Model):
             'primary_muscle': self.primary_muscle,
             'secondary_muscle': self.secondary_muscle
         }
+
+class SignUp(FlaskForm):
+    username = StringField('username', validators=[InputRequired()])
+    email = EmailField('email', validators=[Email(), InputRequired()])
+    password = PasswordField('New Password', validators=[InputRequired(), EqualTo('confirm', message='Passwords must match')])
+    confirm  = PasswordField('Repeat Password')
+    submit = SubmitField('Sign Up', render_kw={'class': 'btn waves-effect waves-light white-text'})
+
+class LogIn(FlaskForm):
+    username = StringField('username', validators=[InputRequired()])
+    password = PasswordField('New Password', validators=[InputRequired()])
+    submit = SubmitField('Login', render_kw={'class': 'btn waves-effect waves-light white-text'})
